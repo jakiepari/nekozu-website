156f2be7f33fe0f0b4a50d40b627a7c448f1408a
jakie <nekozux@gmail.com>
2024-11-29 20:49:46 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Nekozu Web Page",
+  title: "Nekozu Network",
   description: "Nekozu Website!",
 };
 
